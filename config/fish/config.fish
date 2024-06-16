152db234e8e94685d669af5e74f1db1f92db507c
Tisauuur <128767187+Tisauuur@users.noreply.github.com>
2024-06-16 16:00:06 -0400

    add config files
    - Alacritty
    - Fish
    - Tmux
    - Nvim

---
+++ config/fish/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Base pkgs config
+set fish_greeting
+zoxide init fish | source
+
+
+# Aliases
+alias cd="z"
+alias vim="nvim"
+alias vi="nvim"
